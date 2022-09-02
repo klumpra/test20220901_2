4fb27af3f8c35c99718f9a607fcb5df2cbc5184a
klumpra <klumpra@lewisu.edu>
2022-09-01 20:10:22 -0500

    This is the first commit

---
+++ FirstProgram/src/MainProgram.java
@@ -0,0 +1,10 @@
+
+public class MainProgram {
+	public static void main(String[] args) {
+		int x = 7;
+		double y = 3.32;
+		String name = "Ray";
+		double ans = x * y;
+		System.out.println("Ray says the answer is " + ans + ".");
+	}
+}
